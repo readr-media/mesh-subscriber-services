e724861eef6d417af5538b31f4df45aab5ab55ef
Dylan Liu <gaespa007648@gmail.com>
2024-09-26 14:32:02 +0800

    feat: mongodb update user comment

--- src/action/comment.py
+++ src/action/comment.py
@@ -1,6 +1,7 @@
 import datetime
 from gql import gql
-
+import os
+from src.mongo import connect_db, add_comment, remove_comment, edit_comment
 
 def add_comment_mutation(content, gql_client):
     memberId = content.get('memberId', -1)
@@ -36,6 +37,16 @@ def add_comment_mutation(content, gql_client):
             }''' % (memberId, obj, targetId, state, published_date, comment_content)
     result = gql_client.execute(gql(mutation))
     if isinstance(result, dict) and 'createComment' in result:
+        # synchronize mongodb
+        try:
+            commentId = result['createComment']['id']
+            mongo_url = os.environ.get('MONGO_URL', None)
+            env = os.environ.get('ENV', 'dev')
+            if obj=='story' and targetId:
+                db = connect_db(mongo_url, env)
+                add_comment(db, memberId, commentId, targetId, comment_content)
+        except Exception as e:
+            print(f"mongo add_comment failed: {str(e)}")
         return True
     return False
 
@@ -48,11 +59,27 @@ def rm_comment_mutation(content, gql_client):
     mutation = '''
          mutation{
             updateComment(where:{id:%s}, data:{is_active:false}){
-                id
+                    id
+                    member{
+                        id
+                    }
                 }
             }''' % (commentId)
     result = gql_client.execute(gql(mutation))
-    return True if isinstance(result, dict) and 'updateComment' in result else False
+    if isinstance(result, dict) and 'updateComment' in result:
+        # synchronize mongodb
+        try:
+            memberId  = result['updateComment']['member']['id']
+            commentId = result['createComment']['id']
+            mongo_url = os.environ.get('MONGO_URL', None)
+            env = os.environ.get('ENV', 'dev')
+            if commentId:
+                db = connect_db(mongo_url, env)
+                remove_comment(db, memberId, commentId)
+        except Exception as e:
+            print(f"mongo remove_comment failed: {str(e)}")
+        return True
+    return False
 
 def edit_comment_mutation(content, gql_client):
     commentId = content['commentId'] if 'commentId' in content and content['commentId'] else False
@@ -64,11 +91,27 @@ def edit_comment_mutation(content, gql_client):
     mutation = '''
         mutation{
             updateComment(where:{id:%s}, data:{content:"%s", is_edited:true}){
-                id
+                    id
+                    member{
+                        id
+                    }
                 }
             }''' % (commentId, comment_content)
     result = gql_client.execute(gql(mutation))
-    return True if isinstance(result, dict) and 'updateComment' in result  else False
+    if isinstance(result, dict) and 'updateComment' in result:
+        # synchronize mongodb
+        try:
+            memberId  = result['updateComment']['member']['id']
+            commentId = result['createComment']['id']
+            mongo_url = os.environ.get('MONGO_URL', None)
+            env = os.environ.get('ENV', 'dev')
+            if commentId:
+                db = connect_db(mongo_url, env)
+                edit_comment(db, memberId, commentId, comment_content)
+        except Exception as e:
+            print(f"mongo edit_comment failed: {str(e)}")
+        return True
+    return False
 
 def comment_handler(content, gql_client):
 
