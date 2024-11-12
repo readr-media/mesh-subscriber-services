fbf7dde8bf31e4f20953a6465357af18a460e091
Dylan Liu <gaespa007648@gmail.com>
2024-11-12 15:58:14 +0800

    refactor: handler_status

--- src/action/collection.py
+++ src/action/collection.py
@@ -21,24 +21,30 @@ def collection_handler(content):
     memberId = content.get('memberId', config.CUSTOME_MEMBER)
     collectionId = content.get('collectionId', None)
     action = content.get('action', None)
+    handler_status = False
     if memberId == 'customId' or int(memberId)<0:
         print("member is visitor")
-        return False
+        return handler_status
     if collectionId==None:
         print("no required collectionId for action")
-        return False
+        return handler_status
 
-    if action=="add_collection":
-        data, _ = gql_query(MESH_GQL_ENDPOINT, gql_single_collection.format(ID=collectionId))
-        collection = data['collection']
+    try:
+        if action=="add_collection":
+            data, _ = gql_query(MESH_GQL_ENDPOINT, gql_single_collection.format(ID=collectionId))
+            collection = data['collection']
 
-        status = collection.get('status', None)
-        if status=="publish":
-            doc = [{
-                "id": collection['id'],
-                "title": collection['title']
-            }]
-            add_document(config.MEILISEARCH_COLLECTION_INDEX, doc)
-    if action=="remove_collection":
-        del_document(config.MEILISEARCH_COLLECTION_INDEX, collectionId)
-    return True
+            status = collection.get('status', None)
+            if status=="publish":
+                doc = [{
+                    "id": collection['id'],
+                    "title": collection['title']
+                }]
+                add_document(config.MEILISEARCH_COLLECTION_INDEX, doc)
+            handler_status = True
+        if action=="remove_collection":
+            del_document(config.MEILISEARCH_COLLECTION_INDEX, collectionId)
+            handler_status = True
+    except Exception as e:
+        print("collection_handler: ", str(e))
+    return handler_status
